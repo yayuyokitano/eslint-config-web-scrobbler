1e3ce13581e310473881bb49102905d3633f2d21
alexesprit <alex.esprit@gmail.com>
2021-01-23 11:46:05 +0300

    Remove style-related rules
    Code style is handled by Prettier.

--- config/base-rules.js
+++ config/base-rules.js
@@ -10,10 +10,6 @@ module.exports = {
 		 * Possible errors
 		 */
 
-		// Enforce "for" loop update clause moving the counter
-		// in the right direction
-		'for-direction': 'error',
-
 		// Allow unnecessary parentheses
 		'no-extra-parens': 'off',
 
@@ -55,76 +51,20 @@ module.exports = {
 		 * Stylistic issues
 		 */
 
-		// Require 'one true brace style', in which the opening brace
-		// of a block is placed on the same line as its corresponding
-		// statement or declaration
-		'brace-style': ['error', '1tbs'],
-
-		// Disallow spaces inside of brackets
-		'array-bracket-spacing': ['error', 'never'],
-
 		// Require CamelCase
 		camelcase: ['error', { properties: 'never' }],
-
-		// Require or disallow trailing commas
-		'comma-dangle': [
-			'error',
-			{
-				arrays: 'always-multiline',
-				objects: 'always-multiline',
-				imports: 'always-multiline',
-				exports: 'never',
-				functions: 'never',
-			},
-		],
-
-		// Require space after comma
-		'comma-spacing': ['error', { after: true }],
-
-		// Require newline at the end of files
-		'eol-last': ['error', 'always'],
-
-		// Disallow spacing between function identifiers and their invocations
-		'func-call-spacing': 'error',
-
-		// Use tabs as indentation
-		// Enforce indentation level for case clauses in switch statements
-		indent: ['error', 'tab', { SwitchCase: 1 }],
-
-		// Require space after colon in object literal properties
-		'key-spacing': ['error', { afterColon: true }],
-
-		// Require space before and after keywords
-		'keyword-spacing': 'error',
-
-		// Require Unix line endings
-		'linebreak-style': ['error', 'unix'],
-
-		// Disallow empty block statements
-		'no-empty': ['error', { allowEmptyCatch: true }],
 
 		// Disallow `if` statements as the only statement in `else` blocks
 		'no-lonely-if': 'error',
 
-		// Disallow multiple spaces
-		'no-multi-spaces': 'error',
-
 		// Disallow nested ternary expressions
 		'no-nested-ternary': 'error',
-
-		// Disallow trailing whitespace at the end of lines
-		'no-trailing-spaces': 'error',
 
 		// Disallow ternary operators when simpler alternatives exist
 		'no-unneeded-ternary': 'error',
 
-		// Disallow whitespace before properties
-		'no-whitespace-before-property': 'error',
-
+		// Enforce variables to be declared separately in functions
 		'one-var': ['error', 'never'],
-
-		// Require spaces inside curly braces
-		'object-curly-spacing': ['error', 'always'],
 
 		// Require single quotes
 		quotes: [
@@ -135,33 +75,8 @@ module.exports = {
 			},
 		],
 
-		// Require space before blocks
-		'space-before-blocks': ['error', 'always'],
-
-		// Disallow a space before function parenthesis
-		'space-before-function-paren': [
-			'error',
-			{
-				named: 'never',
-				anonymous: 'always',
-				asyncArrow: 'always',
-			},
-		],
-
-		// Disallow spaces inside of parentheses
-		'space-in-parens': 'error',
-
-		// Require spacing around infix operators
-		'space-infix-ops': 'error',
-
 		// Enforce consistent spacing after the // or /* in a comment
 		'spaced-comment': 'error',
-
-		// Require semicolon at the end of statement
-		semi: ['error', 'always'],
-
-		// Enforce spacing around colons of switch statements
-		'switch-colon-spacing': ['error', { after: true, before: false }],
 
 		/*
 		 * ECMAScript 6
