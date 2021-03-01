e8a8382dcd1422f5dd034ec0f3bed02ce1f4a58e
alexesprit <alex.esprit@gmail.com>
2021-03-01 21:31:55 +0300

    Add Vue 3 specific rules

--- config/vue-overrides.js
+++ config/vue-overrides.js
@@ -3,5 +3,9 @@ module.exports = {
 	extends: ['plugin:vue/essential'],
 	rules: {
 		'object-shorthand': ['error', 'always'],
+
+		// Vue 3 specifix rules
+		'vue/no-multiple-template-root': 'off',
+		'vue/no-v-model-argument': 'off',
 	},
 };
